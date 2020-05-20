0b060bc3cc5ef2a31fb1fa5aa31a9f3c74269c24
Atman Das <atmanad0001@gmail.com>
2020-05-20 14:31:02 +0530

    Search Fucntionality added, Image size reduced

---
+++ OnlineMusicPlayer/search.js
@@ -0,0 +1,15 @@
+function searchFunc() {
+    var input, ul, li, i;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("kk");
+    li = ul.getElementsByTagName("th");
+    for (i = 0; i < li.length; i++) {
+        if (!li[i].innerHTML.toUpperCase().includes(filter)) {
+            li[i].style.display = "none";
+        }
+        else {
+            li[i].style.display = "";
+        }
+    }
+} 
